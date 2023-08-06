03c76ab17a70ede21efdb91a2a60e69cf57024bb
Mudathir Lawal <nigercode@gmail.com>
2023-08-06 23:45:23 +0100

    feat: Add code for file extension

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ console.log("Alt___\nSchool\n");
 console.log("School of Engineering (Node.js track)");
 console.log("Assignment I Solutions By: Mudathir Lawal\n");
 
-console.log("Q1: " + path.resolve() + "\n");
+const $cwd = path.resolve();
+const $fileName = "/home/Documents/JavaScript_Book.pdf";
+
+console.log("Q1: " + $cwd+ "\n");
 console.log("Q2: " + path.sep + "\n");
-// console.log("Q3: " + path.resolve() + "\n");
+console.log("Q3: " + path.extname($fileName) + "\n");
