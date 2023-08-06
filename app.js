69b8ace0ba32cc5cd46fbf7ff938355cdbf2734d
Mudathir Lawal <nigercode@gmail.com>
2023-08-06 23:26:34 +0100

    feat: Add code to log working dir

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
 
+console.log("Alt___\nSchool\n");
+console.log("School of Engineering (Node.js track)");
+console.log("Assignment I Solutions By: Mudathir Lawal\n");
 
-console.log("App runs right here...");
+console.log("Q1: " + path.resolve());
+
